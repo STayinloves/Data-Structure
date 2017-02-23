32ad33228856cef2bb2d31116315a99567f70304
stayinloves <mestayinloves@gmail.com>
2017-02-23 16:28:47 +0800

    update

---
+++ java/TestPoint.java
@@ -0,0 +1,16 @@
+class Point<T> {
+    private T x;
+    private T y;
+
+    public Point(T _x, T _y) {
+        this.x = _x;
+        this.y = _y;
+    }
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+    public static void main (String[] args) {
+        Point<Integer> p = new Point<Integer>(1, 2);
+        System.out.println(p);
+    }
+}
